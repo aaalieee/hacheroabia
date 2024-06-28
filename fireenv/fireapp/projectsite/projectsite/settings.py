8bc2282584fd3de43afb057509a9c3492a6179b8
aaalieee <156905811+aaalieee@users.noreply.github.com>
2024-06-27 19:51:00 -0700

    Update settings.py

--- fireenv/fireapp/projectsite/projectsite/settings.py
+++ fireenv/fireapp/projectsite/projectsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-d@4a$whi@(x03p+^ccwzokc$xcs*)qez!1(p_bdyvq1ebk6!3e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'gooofire.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'goofire.pythonanywhere.com']
 
 
 # Application definition
